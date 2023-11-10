70f0f4623eaa9922f2a0b64175a3b16ad4ceee33
Kichin aleksey <kichin21@mail.ru>
2023-11-10 19:30:08 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,157 @@
+# 1
+
+import os
+from datetime import datetime
+
+def logger(old_function):
+
+    def new_function(*args, **kwargs):
+        date_time = datetime.now()
+        result = old_function(*args, **kwargs)
+        with open('main.log', 'a+', encoding='utf-8') as file:
+            file.write(f'{str(date_time)} Вызов функции {str(old_function.__name__)}'\
+                      f' Аргументы позиционные: {args} Аргументы именованные: {kwargs} Результат: {str(result)} \n')
+            return result
+
+    return new_function
+
+
+def test_1():
+    path = 'main.log'
+    if os.path.exists(path):
+        os.remove(path)
+
+    @logger
+    def hello_world():
+        return 'Hello World'
+
+    @logger
+    def summator(a, b=0):
+        return a + b
+
+    @logger
+    def div(a, b):
+        return a / b
+
+    assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+    result = summator(2, 2)
+    assert isinstance(result, int), 'Должно вернуться целое число'
+    assert result == 4, '2 + 2 = 4'
+    result = div(6, 2)
+    assert result == 3, '6 / 2 = 3'
+
+    assert os.path.exists(path), 'файл main.log должен существовать'
+
+    summator(4.3, b=2.2)
+    summator(a=0, b=0)
+
+    with open(path) as log_file:
+        log_file_content = log_file.read()
+
+    assert 'summator' in log_file_content, 'должно записаться имя функции'
+    for item in (4.3, 2.2, 6.5):
+        assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_1()
+# 2
+
+
+
+
+def logger(path):
+    def __logger(old_function):
+
+
+        def new_function(*args, **kwargs):
+            date_time = datetime.now()
+
+
+            result = old_function(*args, **kwargs)
+            with open(path, 'a+', encoding='utf-8') as file:
+                file.write(f'{str(date_time)} Вызов функции {str(old_function.__name__)}'\
+                           f' Аргументы позиционные: {args} Аргументы именованные: {kwargs} Результат: {str(result)} \n')
+            return result
+
+        return new_function
+
+    return __logger
+
+
+def test_2():
+    paths = ('log_1.log', 'log_2.log', 'log_3.log')
+
+    for path in paths:
+        if os.path.exists(path):
+            os.remove(path)
+
+        @logger(path)
+        def hello_world():
+            return 'Hello World'
+
+        @logger(path)
+        def summator(a, b=0):
+            return a + b
+
+        @logger(path)
+        def div(a, b):
+            return a / b
+
+        assert 'Hello World' == hello_world(), "Функция возвращает 'Hello World'"
+        result = summator(2, 2)
+        assert isinstance(result, int), 'Должно вернуться целое число'
+        assert result == 4, '2 + 2 = 4'
+        result = div(6, 2)
+        assert result == 3, '6 / 2 = 3'
+        summator(4.3, b=2.2)
+
+    for path in paths:
+
+        assert os.path.exists(path), f'файл {path} должен существовать'
+
+        with open(path) as log_file:
+            log_file_content = log_file.read()
+
+        assert 'summator' in log_file_content, 'должно записаться имя функции'
+
+        for item in (4.3, 2.2, 6.5):
+            assert str(item) in log_file_content, f'{item} должен быть записан в файл'
+
+
+if __name__ == '__main__':
+    test_2()
+
+# 3
+
+import types
+
+@logger('generator.log')
+def flat_generator(list_of_lists):
+    for inside_list in list_of_lists:
+        for item in inside_list:
+            yield item
+
+
+
+
+def test_3():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_3()
